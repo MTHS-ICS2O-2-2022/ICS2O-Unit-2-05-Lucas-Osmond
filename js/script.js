b232ed79cb0bde349c0576fbe88218d53e017d72
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-03-24 17:31:24 +0000

    finished work

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function calculate() {
+  //Input
+  const hours = parseFloat(document.getElementById('hours-worked-per-week').value)
+  const rate = parseFloat(document.getElementById('dollars-earned-per-hour').value)
+  
+  //Process
+  const TAX_RATE = 0.18
+  const takeHomeSalary = (hours * rate) * (1.00 - TAX_RATE)
+  const taxFraud = (hours * rate) *  TAX_RATE
+
+  //Output
+  document.getElementById('i-get').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+  document.getElementById('government-gets').innerHTML = `Your pay will be: $ ${taxFraud.toFixed(2)}`
+}
